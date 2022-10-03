24dc0896f2138e90c37da7816dce2b6954db79f2
h2lo <olusha.98@mail.ru>
2022-10-03 03:25:29 +0300

    Fix index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,22 @@
 import React from 'react'
-// import ReactDOM from 'react-dom'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
+// import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './p1-main/m1-ui/u1-app/App'
 import * as serviceWorker from './serviceWorker'
 
-// ReactDOM.render(
-//     <React.StrictMode>
-//         <App/>,
-//     </React.StrictMode>,
-//     document.getElementById("root")
-// )
+ReactDOM.render(
+    <React.StrictMode>
+        <App/>,
+    </React.StrictMode>,
+    document.getElementById("root")
+)
 
-const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-);
-root.render(<App />,
-);
+// const root = ReactDOM.createRoot(
+//     document.getElementById('root') as HTMLElement
+// );
+// root.render(<App />,
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
