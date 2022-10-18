fc7e1bf289bcfddc82ecbc4668439faab149a310
h2lo <olusha.98@mail.ru>
2022-10-19 00:07:12 +0300

    Add hw5 task solution

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,12 +1,32 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom';
+import {PATH} from './Pages';
+import s from './Header.module.css'
+
+type ActionType = {
+    [key: string]: boolean
+}
 
 function Header() {
+
+    const navLinkClass = ({isActive}: ActionType) => `${s.navLink} ${isActive ? s.active : s.link}`
+
     return (
-        <div>
-            // add NavLinks
+        <div className={s.headerNavigation}>
+            <NavLink to={PATH.PRE_JUNIOR} className={navLinkClass}>Pre-Junior</NavLink>
+            <NavLink to={PATH.JUNIOR} className={navLinkClass}>Junior</NavLink>
+            <NavLink to={PATH.JUNIOR_PLUS} className={navLinkClass}>Junior+</NavLink>
+            <div className = {s.round}/>
+
+            {/*    <NavLink to={PATH.PRE_JUNIOR} className={navLink => navLink.isActive ? s.active : s.link}>Pre-Junior</NavLink>*/}
+            {/*    <NavLink to={PATH.JUNIOR} className={navLink => navLink.isActive ? s.active : s.link} >Junior</NavLink>*/}
+            {/*    <NavLink to={PATH.JUNIOR_PLUS} className={navLink => navLink.isActive ? s.active : s.link} >Junior+</NavLink>*/}
 
         </div>
     )
 }
 
 export default Header
+
+// activeClassName не работает 6 версии rrd, поэтому пишем через тернарный оператр
+{/*    <NavLink to={PATH.JUNIOR_PLUS} className={s.link} activeClassName = {s.active}>Junior+</NavLink>*/}
